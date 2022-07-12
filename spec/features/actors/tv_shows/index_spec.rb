bf366a0d5cddd0ffc80c1223350c5ee874c09a4d
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-07-11 18:11:34 -0700

    US18 - link to edit info for every tv show on index/actor-tvshow pages

--- spec/features/actors/tv_shows/index_spec.rb
+++ spec/features/actors/tv_shows/index_spec.rb
@@ -18,4 +18,18 @@ it 'can create a new tv show' do
     expect(current_path).to eq("/actors/#{actor_1.id}/tvshows")
     expect(page).to have_content('Curb Your Enthusiasm')
   end  
+
+  it 'has an edit link for every tv show on the actor tv show page' do 
+    actor_1 = Actor.create!(name: "Bob Odenkirk", still_active: true, age: 59)
+    actor_2 = Actor.create!(name: "Bryan Cranston", still_active: true, age: 66)
+    tv_show_1 = actor_1.tv_shows.create!(name: "Better Call Saul", on_air: true, number_of_episodes: 57)
+    tv_show_2 = actor_1.tv_shows.create!(name: "Mr. Show", on_air: false, number_of_episodes: 30)
+    tv_show_3 = actor_2.tv_shows.create!(name: "Breaking Bad", on_air: false, number_of_episodes: 62)
+
+    visit "/actors/#{actor_2.id}/tvshows"
+
+    click_on(id: "show-#{tv_show_3.id}")
+
+    expect(current_path).to eq("/tvshows/#{tv_show_3.id}/edit")
+  end
 end
