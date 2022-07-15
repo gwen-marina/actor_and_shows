c13f605dd68fdaf7aed6f03088cef9405171b05c
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-07-14 17:44:16 -0700

    Updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
   Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'application#welcome'
-  
+  get '/actors', to: 'application#welcome'
+
   get '/actors', to: 'actors#index'
   get '/tvshows', to: 'tv_shows#index'
   get '/tvshows/:id', to: 'tv_shows#show'
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,14 @@ Actor.destroy_all
 
 actor_1 = Actor.create!(name: "Bob Odenkirk", still_active: true, age: 59)
 actor_2 = Actor.create!(name: "Bryan Cranston", still_active: true, age: 66)
+actor_3 = Actor.create!(name: "Rhea Seehorn", still_active: true, age: 48)
+actor_4 = Actor.create!(name: "Aaron Paul", still_active: true, age: 42)
+actor_5 = Actor.create!(name: "Anna Gunn", still_active: true, age: 53)
 
-actor_2.tv_shows.create!(name: "Breaking Bad", on_air: false, number_of_episodes: 62)
 actor_1.tv_shows.create!(name: "Better Call Saul", on_air: true, number_of_episodes: 57)
 actor_1.tv_shows.create!(name: "Mr. Show", on_air: false, number_of_episodes: 30)
+actor_2.tv_shows.create!(name: "Breaking Bad", on_air: false, number_of_episodes: 62)
+actor_3.tv_shows.create!(name: "Better Call Saul", on_air: true, number_of_episodes: 57)
+actor_4.tv_shows.create!(name: "Breaking Bad", on_air: false, number_of_episodes: 62)
+actor_4.tv_shows.create!(name: "Westworld", on_air: true, number_of_episodes: 20)
+actor_5.tv_shows.create!(name: "Breaking Bad", on_air: false, number_of_episodes: 62)
