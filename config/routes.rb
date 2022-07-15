68c334863f3fb856ea7d2fe8e6802a51087d20c0
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-07-14 18:36:40 -0700

    route issue- fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
   Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/actors', to: 'application#welcome'
+  get '/', to: 'actors#index'
 
   get '/actors', to: 'actors#index'
   get '/tvshows', to: 'tv_shows#index'
